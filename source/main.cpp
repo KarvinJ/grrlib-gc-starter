0c4072b439c529449b7d1ca156907742cd3349cd
knight <karvin.jimenez@alternard.com>
2024-09-19 14:22:43 -0400

    improvements to the template.

--- source/main.cpp
+++ source/main.cpp
@@ -1,18 +1,21 @@
 #include <grrlib.h>
-#include <stdlib.h>
+#include <iostream>
 #include <ogc/pad.h>
 #include "BMfont3_png.h"
 // #include "alien_1_png.h"
-#include "test_jpg_jpg.h"
+// #include "test_jpg_jpg.h"
 
 #define BLACK 0x000000FF
 #define WHITE 0xFFFFFFFF
+#define GREEN 0x008000FF
+#define RED 0xFF0000FF
+#define BLUE 0x0000FFFF
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
-const int SPEED = 10;
+const int PLAYER_SPEED = 10;
 
-bool isGamePaused = false;
+bool isGamePaused;
 
 typedef struct
 {
@@ -23,31 +26,71 @@ typedef struct
     unsigned int color;
 } Rectangle;
 
-Rectangle bounds = {0, 0, 64, 64, WHITE};
+Rectangle player = {SCREEN_WIDTH / 2 - 32, SCREEN_HEIGHT / 2 - 32, 64, 64, WHITE};
+
+Rectangle ball = {SCREEN_WIDTH / 2 + 100, SCREEN_HEIGHT / 2 - 32, 32, 32, WHITE};
+
+int ballVelocityX = 4;
+int ballVelocityY = 4;
+
+int score;
+
+bool hasCollision(Rectangle bounds, Rectangle ball)
+{
+    return bounds.x < ball.x + ball.w && bounds.x + bounds.w > ball.x &&
+           bounds.y < ball.y + ball.h && bounds.y + bounds.h > ball.y;
+}
 
 void update()
 {
     const u32 padHeld = PAD_ButtonsHeld(0);
 
-    if (padHeld & PAD_BUTTON_LEFT && bounds.x > 0)
+    if (padHeld & PAD_BUTTON_LEFT && player.x > 0)
     {
-        bounds.x -= SPEED;
+        player.x -= PLAYER_SPEED;
     }
 
-    else if (padHeld & PAD_BUTTON_RIGHT && bounds.x < SCREEN_WIDTH - bounds.w)
+    else if (padHeld & PAD_BUTTON_RIGHT && player.x < SCREEN_WIDTH - player.w)
     {
-        bounds.x += SPEED;
+        player.x += PLAYER_SPEED;
     }
 
-    else if (padHeld & PAD_BUTTON_UP && bounds.y > 0)
+    else if (padHeld & PAD_BUTTON_UP && player.y > 0)
     {
-        bounds.y -= SPEED;
+        player.y -= PLAYER_SPEED;
     }
 
-    else if (padHeld & PAD_BUTTON_DOWN && bounds.y < SCREEN_HEIGHT - bounds.h)
+    else if (padHeld & PAD_BUTTON_DOWN && player.y < SCREEN_HEIGHT - player.h)
     {
-        bounds.y += SPEED;
+        player.y += PLAYER_SPEED;
     }
+
+    if (ball.x < 0 || ball.x > SCREEN_WIDTH - ball.w)
+    {
+        ballVelocityX *= -1;
+
+        ball.color = GREEN;
+    }
+
+    else if (ball.y < 0 || ball.y > SCREEN_HEIGHT - ball.h)
+    {
+        ballVelocityY *= -1;
+
+        ball.color = RED;
+    }
+
+    else if (hasCollision(player, ball))
+    {
+        ballVelocityX *= -1;
+        ballVelocityY *= -1;
+
+        ball.color = BLUE;
+
+        score++;
+    }
+
+    ball.x += ballVelocityX;
+    ball.y += ballVelocityY;
 }
 
 int main(int argc, char **argv)
@@ -59,10 +102,10 @@ int main(int argc, char **argv)
     // GRRLIB_texImg *tex_test_jpg = GRRLIB_LoadTexture(test_jpg_jpg);
 
     // loading fonts
-    GRRLIB_texImg *tex_BMfont3 = GRRLIB_LoadTexture(BMfont3_png);
+    GRRLIB_texImg *blueFonts = GRRLIB_LoadTexture(BMfont3_png);
 
     // To indicate the font region to load.
-    GRRLIB_InitTileSet(tex_BMfont3, 32, 32, 32);
+    GRRLIB_InitTileSet(blueFonts, 32, 32, 32);
 
     // Initialise the GameCube controllers
     PAD_Init();
@@ -96,22 +139,27 @@ int main(int argc, char **argv)
         // Place your drawing code below
         // ---------------------------------------------------------------------
 
+        // Draw a img
+        // GRRLIB_DrawImg(10, 50, alien, 0, 1, 1, WHITE);
+
+        GRRLIB_Rectangle(player.x, player.y, player.w, player.h, player.color, 1);
+        GRRLIB_Rectangle(ball.x, ball.y, ball.w, ball.h, ball.color, 1);
+
         // displaying text with the loaded fonts.
         if (isGamePaused)
         {
-            // Any text has to be written in uppercase with this font.
-            GRRLIB_Printf(150, 20, tex_BMfont3, WHITE, 1, "GAME PAUSED");
+            // Any text has to be written in uppercase if not it doesn't work.
+            GRRLIB_Printf(150, 100, blueFonts, WHITE, 1, "GAME PAUSED");
         }
 
-        // Draw jpg img
-        //  GRRLIB_DrawImg(10, 50, tex_test_jpg, 0, 1, 1, WHITE);
+        std::string scoreString = "SCORE: " + std::to_string(score);
 
-        GRRLIB_Rectangle(bounds.x, bounds.y, bounds.w, bounds.h, bounds.color, 1);
+        GRRLIB_Printf(185, 20, blueFonts, WHITE, 1, scoreString.c_str());
 
         GRRLIB_Render(); // Render the frame buffer to the TV
     }
 
-    GRRLIB_FreeTexture(tex_BMfont3); // Be a good boy, clear the memory allocated by GRRLIB
+    GRRLIB_FreeTexture(blueFonts); // Be a good boy, clear the memory allocated by GRRLIB
 
     GRRLIB_Exit();
     exit(0); // Use exit() to exit a program, do not use 'return' from main()
